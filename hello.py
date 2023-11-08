cdae2bedfd73d10b86daca1e5554cc45a412aee2
조형근 <jhkluck22@naver.com>
2023-11-08 14:44:57 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,26 @@
-def print_3_times():
-    print("hi")
-    print("hi")
-    print("hi")
+# output_a = str(52)
+# output_b = str(52.273)
+# print(type(output_a), output_a)
+# print(type(output_b), output_b) 
 
-print_3_times()
+# string_a = input("입력A> ")
+# int_a = int(string_a)
+
+# string_b = input("입력B> ")
+# int_b = int(string_b)
+
+# print("문자열 자료 :", string_a + string_b)
+# print("숫자 자료 :", int_a + int_b)
+
+# output_a = str(52)
+# output_b = str(52.273)
+# print(type(output_a), output_a)
+# print(type(output_b), output_b)
+
+# 숫자를 입력받습니다.
+raw_input = input("inch 단위의 숫자를 입력해주세요: ")
+
+# 입력받은 데이터를 숫자 자료형으로 변경하고, cm 단위로 변경합니다.
+inch = int(raw_input)
+cm = inch * 2.54
+
