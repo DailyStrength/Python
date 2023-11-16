cda2b1186087ad7cd7ffbeff9f5209ca4fa27c6a
조형근 <jhkluck22@naver.com>
2023-11-16 14:43:55 +0900

    11/16

---
+++ hello1.py
@@ -0,0 +1,13 @@
+# with open('data/chicken.txt', 'r', encoding='UTF-8') as f:
+#     for line in f:
+#         print(line)
+
+# Strip
+# print("      abc   def     ".strip())
+
+# split
+# my_string = "1. 2. 3. 4. 5. 6"
+# print(my_string.split(". "))
+
+full_name = "Kim, Yuna"
+print(full_name.split(", "))
