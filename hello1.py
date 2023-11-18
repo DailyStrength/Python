7693e564067ea931d067fbb96ea36f95de08a67b
조형근 <jhkluck22@naver.com>
2023-11-18 22:37:11 +0900

    휴일이지만 조금이라도 공부함

--- hello1.py
+++ hello1.py
@@ -31,12 +31,56 @@
 # for i, value in enumerate(example_list):
 #     print("{}번째 요소는 {}입니다.".format(i, value))
 
-example_dectionary = {
-    "키A" : "값A",
-    "키B" : "값B",
-    "키C" : "값C"
-}
+# example_dectionary = {
+#     "키A" : "값A",
+#     "키B" : "값B",
+#     "키C" : "값C"
+# }
 
-print("# 딕셔너리의 items() 함수")
-print("items():", example_dectionary.items())
-print()
+# print("# 딕셔너리의 items() 함수")
+# print("items():", example_dectionary.items())
+# print()
+
+# my_string = "1. 2. 3. 4. 5. 6"
+# print(my_string.split("."))
+
+# full_name = "Kim, Yuna"
+# name_data = print(full_name.split(", "))
+
+# print("     \n\n    3   \t  3   \n 5 7 11 \n\n".split())
+
+# full_name = "Kim, Yuna"
+# name_data = full_name.split(", ")
+# last_name = name_data[0]
+# first_name = name_data[1]
+
+# # print(first_name, last_name)
+# day = []
+
+# with open('data/chicken.txt', 'r', encoding='UTF-8') as f:
+#     data = f.readlines()
+
+# total_sales = 0
+# days = 0
+
+# for line in data:
+#     day, sales = line.strip().split(":")
+#     total_sales += int(sales)
+#     days += 1
+
+# average_sales = total_sales / days
+# print("12월 코빠닭의 하루 평균 매출:", average_sales)
+
+# revenue = 0
+# day_revenue = 0
+# day = 0
+
+# with open('data/chicken.txt', 'r', encoding = 'UTF-8') as in_file:
+#     for line in in_file:
+#         data = line.strip().split(":")
+#         revenue += int(data[1])
+#         day += 1
+#     print(revenue)
+# day_revenue = revenue / day
+# print("12월 매출 평균은 : ",day_revenue)
+
---
+++ hello2.py
@@ -0,0 +1,12 @@
+# revenue = 0
+# day_revenue = 0
+# day = 0
+
+# with open('data/chicken.txt', 'r', encoding = 'UTF-8') as in_file:
+#     for line in in_file:
+#         data = line.strip().split(":")
+#         revenue += int(data[1])
+#         day += 1
+#     print(revenue)
+# day_revenue = revenue / day
+# print("12월 매출 평균은 : ",day_revenue)
