33cd86ff8ac28e6c7f20f2ee99c2c9faf3370ba6
조형근 <jhkluck22@naver.com>
2023-11-18 21:46:54 +0900

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -9,5 +9,34 @@
 # my_string = "1. 2. 3. 4. 5. 6"
 # print(my_string.split(". "))
 
-full_name = "Kim, Yuna"
-print(full_name.split(", "))
+# full_name = "Kim, Yuna"
+# print(full_name.split(", "))
+
+# list_a = [1, 2, 3, 4, 5]
+# list_reversed = reversed(list_a)
+
+# print("# reversed() method")
+# print("reversed([1, 2, 3, 4, 5]):", list_reversed)
+# print("list(reversed([1, 2, 3, 4, 5])):", list(list_reversed))
+# print()
+
+# print("# reversed() 함수와 반복문")
+# print("for i in reersed([1, 2, 3, 4, 5]):")
+# for i in  reversed(list_a):
+#     print("-",i)
+
+# example_list = ["요소A", "요소B", "요소C"]
+
+# print("# 반복문과 조합하기")
+# for i, value in enumerate(example_list):
+#     print("{}번째 요소는 {}입니다.".format(i, value))
+
+example_dectionary = {
+    "키A" : "값A",
+    "키B" : "값B",
+    "키C" : "값C"
+}
+
+print("# 딕셔너리의 items() 함수")
+print("items():", example_dectionary.items())
+print()
