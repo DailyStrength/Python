8f4911984fcb8e4255ab25754b95179918fd79d2
DailyStrength <jhkluck22@gmail.com>
2025-08-31 19:45:46 +0900

    first commit for coding test

---
+++ 1st_week/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
+# print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
+# print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
